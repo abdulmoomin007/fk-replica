69a3c01b502d73ce986032019d77e6e3366091d4
abdulmoomin007 <abdulmoomin007@gmail.com>
2020-04-28 01:10:13 +0530

    fixed bugs and added js-code

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementById("electronics").addEventListener("mouseover",function(){
+    console.log("Electronics Clicked")
+})
+
+document.getElementById("tv-appliances").addEventListener("mouseover",function(){
+    console.log("TVs and Appliances Clicked")
+})
+
+document.getElementById("men").addEventListener("mouseover",function(){
+    console.log("Men Clicked")
+})
+
+document.getElementById("women").addEventListener("mouseover",function(){
+    console.log("Women Clicked")
+})
+
+document.getElementById("baby").addEventListener("mouseover",function(){
+    console.log("Baby and Kids Clicked")
+})
+
+document.getElementById("sports").addEventListener("mouseover",function(){
+    console.log("Sports, Books and More Clicked")
+})
